8fe04c5702c71e3a81ee017a7b46ff97b89c2dc0
Your Name <you@example.com>
2026-07-26 12:58:29 +0200

    change gtm

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -141,7 +141,9 @@ export default async function LocaleLayout({ children, params }) {
   return (
     <html lang={locale} className={`${inter.variable} ${manrope.variable}`}>
       <head>
-        <script
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
